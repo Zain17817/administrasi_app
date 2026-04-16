09d9e44a598a9e4738c7e4710b98793c5933f725
zain17817 <ishlahsoko@gmail.com>
2026-04-16 14:14:52 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,9 @@ use App\Http\Controllers\PengajuanController;
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-<<<<<<< HEAD
-// =======================
-// ROUTE PUBLIK
-// =======================
-Route::get('/', [PengajuanController::class, 'create'])->name('pengajuan.form');
-=======
 // Route publik
 // Route::get('/, [PengajuanController::class, 'create'])->name('pengajuan.landing');
 Route::get('/pengajuan', [PengajuanController::class, 'create'])->name('pengajuan.form');
->>>>>>> 2b1d437735edf109d94ea976914d3cbfcbbe91d3
 Route::post('/pengajuan', [PengajuanController::class, 'store'])->name('pengajuan.store');
 
 Route::get('/cek-status', [PengajuanController::class, 'cekStatusForm'])->name('pengajuan.cek-status');
@@ -30,18 +23,10 @@ Route::prefix('admin')->group(function () {
     Route::post('/login', [AdminController::class, 'login']);
     Route::post('/logout', [AdminController::class, 'logout'])->name('admin.logout');
 
-<<<<<<< HEAD
-    // area admin (protected)
-=======
->>>>>>> 2b1d437735edf109d94ea976914d3cbfcbbe91d3
     Route::middleware('admin')->group(function () {
         Route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
         Route::get('/detail/{id}', [AdminController::class, 'detail']);
         Route::post('/detail/{id}', [AdminController::class, 'updateStatus']);
         Route::get('/download/{file}', [AdminController::class, 'download']);
     });
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 2b1d437735edf109d94ea976914d3cbfcbbe91d3
